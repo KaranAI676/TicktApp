ac6f59a07fbe40e378ef59804fa3121925229294
Karan <karan@appinventiv.com>
2022-05-05 17:29:27 +0530

    password key maintained as a constant

--- Tickt/Helper/AppUtilities/AppConstants.swift
+++ Tickt/Helper/AppUtilities/AppConstants.swift
@@ -61,6 +61,7 @@ let kAppDelegate = UIApplication.shared.delegate as! AppDelegate
 //}
 
 enum AppConstants : String {
+    case accessTokenPass = "tickt_app_123sadefss"
 //    case poolIDAWS = "us-east-1:b1f250f2-66a7-4d07-96e9-01817149a439"
 	case googleAPIKey = "AIzaSyCYVGa50KuwOcTs35CsRYUjMpHZ7zS_-VY"//"AIzaSyAB0RM8qn8nTq5p-0EoqeHEiRYmz7yRUG0" //"AIzaSyAw7-pyEPZIjisG8q4YHHjUXji3Xcfk4nw"
     case googlePlaceKey = "AIzaSyD9xVqVmWv1Z-L9WxWt4MUTwhoK5nhGI_w"
@@ -75,6 +76,7 @@ enum AppConstants : String {
     //InterCom
     case getIntercomApiKey = "ios_sdk-11f041b6eb37d470561d82d0db6d9b74c1a101f6"
     case getIntercomAppId = "tvbm4bhr"
+    
 }
 
 
